71fde65b25d50def0ead1f980fa261937d47aa71
miguel.desktop <miguelfm.pro@gmail.com>
2021-02-12 22:04:49 +0800

    updated nav css and js

--- apps.js
+++ apps.js
@@ -1,27 +1,26 @@
 const jsHeader = document.querySelector('header');
 const jsNavlink_02 = document.querySelector('.nav__link-02');
 const jsLa2_li = document.querySelectorAll('dd_nav-link');
-const jsBotBr = document.querySelector('.nav')
+const jsBotBr = document.querySelector('.nav');
+const jsBotBr__linkText = document.getElementsByClassName('nav__li--01');
 
-
-let mediaQuery  = window.matchMedia('(min-width:680px)');
+let mediaQuery01  = window.matchMedia('(min-width:680px)');
 function handleTabletChange(e) {
   // Check if the media query is true
   if (e.matches) {
     if (jsNavlink_02.classList.contains('show') || jsHeader.classList.contains('expand')){
       jsNavlink_02.classList.remove('show');
       jsHeader.classList.remove('expand');
-      jsBotBr.classList.remove('botbr')
+      jsBotBr.classList.remove('botbr');
     }
     // console.log("jsHeader.classList.remove('open')");
+      jsBotBr.style.backgroundColor = "transparent";
   }
 }
 // Register event listener
-mediaQuery.addEventListener('change',handleTabletChange)
+mediaQuery01.addEventListener('change',handleTabletChange)
 // Initial check
-handleTabletChange(mediaQuery)
-
-
+handleTabletChange(mediaQuery01)
 const jsHamburger = document.querySelector('.nav__hamburger');
 jsHamburger.addEventListener('click',()=>{ //< this function run when the hamburger is clicked
   jsHeader.classList.toggle('expand'); //< just auto height
@@ -29,3 +28,35 @@ jsHamburger.addEventListener('click',()=>{ //< this function run when the hambur
   // jsNavCon.classList.toggle("open"); //< this remove the bottom border radius
   jsBotBr.classList.toggle('botbr');
 })
+
+
+let changeFontColorToWhite =  function(){
+  for(let i = 0; i < jsBotBr__linkText.length; i++){
+      jsBotBr__linkText[i].style.color = "white";
+      // console.log(jsBotBr__linkText);
+    }
+};
+let changeFontColorToBlack =  function(){
+  for(let i = 0; i < jsBotBr__linkText.length; i++){
+      jsBotBr__linkText[i].style.color = "black";
+      // console.log(jsBotBr__linkText);
+    }
+};
+
+// transparent to colored nav
+let mediaQuery02 = window.matchMedia('(min-width: 800px)');
+mediaQuery02.addEventListener('onscroll',myFunction)
+window.onscroll = function() {myFunction(mediaQuery02)};
+function myFunction(windowSize) {
+    if(windowSize.matches){
+      if (document.documentElement.scrollTop > 20) {
+        jsBotBr.style.backgroundColor = "rgb(20, 40, 80)";
+        changeFontColorToWhite();
+      } else if(document.documentElement.scrollTop <= 20) {
+        jsBotBr.style.backgroundColor = "transparent";
+        changeFontColorToBlack();
+      }
+        
+    }else {jsBotBr.style.backgroundColor = "transparent";}
+  }
+
